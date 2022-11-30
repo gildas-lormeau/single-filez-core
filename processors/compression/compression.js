c2b59df682a676648ec53e80851aae161dee2265
Gildas <gildas.lormeau@gmail.com>
2022-11-30 20:03:10 +0100

    fix self-extraction issue

--- processors/compression/compression.js
+++ processors/compression/compression.js
@@ -37,7 +37,7 @@ import {
 	display
 } from "./compression-display.js";
 
-const { Blob, fetch, TextEncoder, DOMParser, stop } = globalThis;
+const { Blob, fetch, TextEncoder, DOMParser } = globalThis;
 
 const NO_COMPRESSION_EXTENSIONS = [".jpg", ".jpeg", ".png", ".pdf", ".woff2", ".mp4", ".mp3", ".ogg", ".webp", ".webm"];
 const SCRIPT_PATH = "/lib/single-file-zip.min.js";
@@ -146,7 +146,7 @@ async function addFile(zipWriter, prefixName, data) {
 }
 
 async function getContent() {
-	const { XMLHttpRequest, document, setTimeout, clearTimeout } = globalThis;
+	const { XMLHttpRequest, document, setTimeout, clearTimeout, stop } = globalThis;
 	const xhr = new XMLHttpRequest();
 	let displayTimeout;
 	Array.from(document.documentElement.childNodes).forEach(node => {
