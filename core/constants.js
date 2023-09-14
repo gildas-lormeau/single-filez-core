48546fe96f7b7a78d02b077515e071f899dc7479
Gildas <gildas.lormeau@gmail.com>
2023-09-14 23:25:38 +0200

    add `MESSAGE_PREFIX` constant

--- core/constants.js
+++ core/constants.js
@@ -2,10 +2,12 @@ const SINGLE_FILE_PREFIX = "single-filez-";
 const COMMENT_HEADER = "Page saved with SingleFileZ";
 const SINGLE_FILE_SIGNATURE = "SingleFileZ";
 const WAIT_FOR_USERSCRIPT_PROPERTY_NAME = "_singleFileZ_waitForUserScript";
+const MESSAGE_PREFIX = "__sfz_frameTree__::";
 
 export {
 	SINGLE_FILE_PREFIX,
 	COMMENT_HEADER,
 	SINGLE_FILE_SIGNATURE,
-	WAIT_FOR_USERSCRIPT_PROPERTY_NAME
+	WAIT_FOR_USERSCRIPT_PROPERTY_NAME,
+	MESSAGE_PREFIX
 };
