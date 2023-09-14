8e6c54ff688a5c3b79d09a91d26b9a2bb5aa8826
Gildas <gildas.lormeau@gmail.com>
2023-09-14 22:53:47 +0200

    add `constants.js`

---
+++ core/constants.js
@@ -0,0 +1,9 @@
+const SINGLE_FILE_PREFIX = "single-filez-";
+const COMMENT_HEADER = "Page saved with SingleFileZ";
+const SINGLE_FILE_SIGNATURE = "SingleFileZ";
+
+export {
+	SINGLE_FILE_PREFIX,
+	COMMENT_HEADER,
+	SINGLE_FILE_SIGNATURE
+};
