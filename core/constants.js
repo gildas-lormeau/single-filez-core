1d4a7f399161edd82cd7314071aad2233c9e75e2
Gildas <gildas.lormeau@gmail.com>
2023-09-30 03:05:54 +0200

    add constant

--- core/constants.js
+++ core/constants.js
@@ -3,11 +3,13 @@ const COMMENT_HEADER = "Page saved with SingleFileZ";
 const SINGLE_FILE_SIGNATURE = "SingleFileZ";
 const WAIT_FOR_USERSCRIPT_PROPERTY_NAME = "_singleFileZ_waitForUserScript";
 const MESSAGE_PREFIX = "__sfz_frameTree__::";
+const NO_SCRIPT_PROPERTY_NAME = "singleFileDisabledNoscript";
 
 export {
 	SINGLE_FILE_PREFIX,
 	COMMENT_HEADER,
 	SINGLE_FILE_SIGNATURE,
 	WAIT_FOR_USERSCRIPT_PROPERTY_NAME,
-	MESSAGE_PREFIX
+	MESSAGE_PREFIX,
+	NO_SCRIPT_PROPERTY_NAME
 };
