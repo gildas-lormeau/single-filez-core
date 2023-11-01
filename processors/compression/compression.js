2105ab01a050e0aa97855b7b064ae2a0e6a58059
Gildas <gildas.lormeau@gmail.com>
2023-11-01 02:49:11 +0100

    fix script

--- processors/compression/compression.js
+++ processors/compression/compression.js
@@ -117,12 +117,12 @@ async function process(pageData, options, lastModDate = new Date()) {
 		script = "<script>" +
 			script +
 			"document.currentScript.remove();" +
-			"globalThis.onload = () => {" +
-			"globalThis.bootstrap=(()=>{let bootstrapStarted;return async content=>{if (bootstrapStarted) return bootstrapStarted;bootstrapStarted = (" +
+			"globalThis.addEventListener('load', () => {" +
+			"globalThis.bootstrap=(()=>{let bootstrapStarted;return async content=>{if (bootstrapStarted) return bootstrapStarted; bootstrapStarted = (" +
 			extract.toString().replace(/\n|\t/g, "") + ")(content,{prompt}).then(({docContent}) => " +
 			display.toString().replace(/\n|\t/g, "") + "(document,docContent));return bootstrapStarted;}})();(" +
-			getContent.toString().replace(/\n|\t/g, "") + ")().then(globalThis.bootstrap).catch(()=>{});" +
-			"};" +
+			getContent.toString().replace(/\n|\t/g, "") + ")().then(globalThis.bootstrap).then(() => document.dispatchEvent(new CustomEvent(\"single-file-display-infobar\"))).catch(()=>{});" +
+			"});" +
 			"</script>";
 		pageContent += script;
 		let extraData = "";
